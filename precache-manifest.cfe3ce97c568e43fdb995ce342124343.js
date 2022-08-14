8cd25de7b8b36eb0ed0849ea9b46fff33e182f84
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2022-08-14 16:51:36 +0800

    Updates

---
+++ precache-manifest.cfe3ce97c568e43fdb995ce342124343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf33c7b359204794383a6c253ca7df6",
+    "url": "/Realtime-Multi-Object-Detection/index.html"
+  },
+  {
+    "revision": "18f212aa201510f9638b",
+    "url": "/Realtime-Multi-Object-Detection/static/css/main.267b3af5.chunk.css"
+  },
+  {
+    "revision": "1731422fdae5daa2bd23",
+    "url": "/Realtime-Multi-Object-Detection/static/js/2.d96d903f.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/Realtime-Multi-Object-Detection/static/js/2.d96d903f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f212aa201510f9638b",
+    "url": "/Realtime-Multi-Object-Detection/static/js/main.b1b36b3e.chunk.js"
+  },
+  {
+    "revision": "fbf8989cf0c461225487",
+    "url": "/Realtime-Multi-Object-Detection/static/js/runtime-main.b536f0b1.js"
+  }
+]);
